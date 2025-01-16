13a085f417cb8d2bd91435fdd233e81e6851036f
Gayathiri_Devi <gayathiridevi4796@gmail.com>
2025-01-16 12:58:46 +0000

    commit -m getting random value from the list

---
+++ random_name_from_list.py
@@ -0,0 +1,9 @@
+import  random
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+# # option 1
+# random = random.randint(0,4)
+# print(friends[random])
+
+# option 2
+print(random.choice(friends))
+
